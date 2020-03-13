d1b3a43e055f5d3726db208f78f054776ed4625f
Galay Yuriy <y4109galay@gmail.com>
2020-03-14 01:00:37 +0200

    Have a problem

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const { Worker, parentPort } = require('worker_threads');
+const {Worker, isMainThread, parentPort } = require('worker_threads');
+const os = require('os').cpus(); //для правильного подсчета количества воркеров
 
 const randomData = require('./randomizeData'); //Рандомно сгенерированный массив с объектами
 
@@ -22,25 +23,25 @@ function getSlicedArr(arr, subArrSize, slicedArr = []){
     return slicedArr;
 }
 
+//Функция для поиска объекта в одном массиве
+function search(arr, x){
+    //Если последний элемент не удовлетворяет искомые значения
+    if (!findIn(x, arr[arr.length - 1])){
+        for (let i = 0; i < arr.length; i++){
+            if (findIn(x, arr[i]) && i < arr.length - 1) return arr[i];
+        }
+    }else return arr[arr.length - 1];
+}
+
 //Функция для сравнения свойств главного и искомого объектов
 function findIn(sourceObject, targetObject) {
     return !Object.keys(sourceObject).some(s => sourceObject[s] !== targetObject[s]) ? targetObject.num : false;
 }
 
-//Функция для поиска объекта
-function search(arr, x){
-    //Если последний элемент не удовлетворяет искомые значения
-    if (!findIn(x, arr[arr.length - 1])){
-        for (let i = 0; i < arr.length; i++){
-           if (findIn(x, arr[i]) && i < arr.length - 1) return arr[i];
-        }
-    }else return arr[arr.length - 1];
-}
-
 //НАЧАЛО РАБОТЫ С ПОТОКАМИ
-function evalInWorker(){
-    return new Promise((res, rej) =>{
-        try {
+function evalInWorker(f){
+    if (isMainThread){
+        return new Promise((res, rej) =>{
             const worker = new Worker(__filename, {eval: true});
             worker.on('error', e => rej(e));
             worker.on('message', msg => {
@@ -48,10 +49,21 @@ function evalInWorker(){
             });
             worker.on('exit', code => {
                 if(code !== 0)
-                   rej(new Error(`Worker stopped with exit code ${code}`));
+                    rej(new Error(`Worker stopped with exit code ${code}`));
             });
-        }catch (e) {
-            rej(e);
-        }
-    });
+        });
+    }else {
+        parentPort.postMessage(f());
+    }
 }
+//getSlicedArr возвращает массив с подмассивами, search - ищет в подмассиве объект по нужным свойствам needToFind
+const tasks = (threads, dataArr, needToFind, arr = []) => {
+    getSlicedArr(dataArr, threads).map( e => arr.push(evalInWorker(search(e, needToFind))));
+    return arr;
+};
+
+Promise.all(tasks(subArrSize, dataArr, needToFind))
+    .then(messList => {
+        messList.forEach(m => console.log(m))
+    })
+    .catch(e => console.log(e));
